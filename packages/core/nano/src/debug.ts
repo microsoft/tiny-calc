b3a2537e5b0365a7d4b653cac490a83e8c49cbbc
Jack Williams <jw@jackw.io>
2019-05-21 21:47:12 +0100

    Upgrade parser (#8)

--- packages/core/nano/src/debug.ts
+++ packages/core/nano/src/debug.ts
@@ -1,3 +1,13 @@
 export function error(message: string): never {
-    throw message;
+    throw new Error(message);
+}
+
+export function assertNever(proof: never, message: string = "assertNever violation"): never {
+    return error(message);
+}
+
+export function assert(expression: boolean, message = "False expression"): void {
+    if (!expression) {
+        return error(message);
+    }
 }
