58af276dfc83a592a4780a11056e633239ad7baa
Daniel Gil <dangil@microsoft.com>
2019-06-13 15:28:55 -0700

    Fix single quotes in index.ts

--- packages/core/nano/src/index.ts
+++ packages/core/nano/src/index.ts
@@ -1,2 +1,2 @@
-export { compile, Formula, CalcValue, CalcObj, CalcFun, Primitive } from './compiler';
-export { IDataProvider, IDataObserver, IDataResult, IData } from './types';
+export { compile, Formula, CalcValue, CalcObj, CalcFun, Primitive } from "./compiler";
+export { IDataProvider, IDataObserver, IDataResult, IData } from "./types";
