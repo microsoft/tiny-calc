f2c5955ed27089531b0092dfa0344779c26f960b
Jack Williams <jw@jackw.io>
2019-06-18 10:27:43 +0100

    JW changes

--- packages/core/nano/src/types.ts
+++ packages/core/nano/src/types.ts
@@ -4,6 +4,20 @@ export interface Pending<T> {
 }
 
 /**
+ * The interface for an object that can bind to an IProducer.
+ *
+ * Any object that implements IConsumer is expected to provide a
+ * callback whenever the component it is bound to changes in value and a reference
+ * to the data that the consumer is bound to.
+ */
+export interface IConsumer<T> {
+  /**
+   * Invoked whenever the data this object is bound to is changed.
+   */
+  valueChanged<U extends T, K extends keyof U>(property: K, value: U[K], producer: IProducer<U>): void;
+}
+
+/**
  * The interface for an object whose data can be bound to. We use this contract for
  * components that want to expose their data and its changes to other components.
  *
@@ -32,23 +46,9 @@ export interface IProducer<T> {
   }
 }
 
-/**
- * The interface for an object that can bind to an IProducer.
- *
- * Any object that implements IConsumer is expected to provide a
- * callback whenever the component it is bound to changes in value and a reference
- * to the data that the consumer is bound to.
- */
-export interface IConsumer<T> {
-  /**
-   * Invoked whenever the data this object is bound to is changed.
-   */
-  valueChanged<U extends T, K extends keyof U>(producer: IProducer<U>, property: K, value: U[K]): void;
-}
-
 export interface IVectorConsumer<T> {
   /** Notification that a range of items have been inserted, removed, and/or replaced in the given vector. */
-  itemsChanged(producer: IVectorProducer<T>, index: number, numRemoved: number, itemInserted: T[]): void;
+  itemsChanged(index: number, numRemoved: number, itemInserted: T[], producer: IVectorProducer<T>): void;
 }
 
 /** Provides more efficient access to 1D data for vector-aware consumers. */
@@ -67,13 +67,13 @@ export interface IVectorProducer<T> {
 
 export interface IMatrixConsumer<T> {
   /** Notification that rows have been inserted, removed, and/or replaced in the given matrix. */
-  rowsChanged(producer: IMatrixProducer<T>, row: number, numRemoved: number, rowsInserted: T[]): void;
+  rowsChanged(row: number, numRemoved: number, rowsInserted: T[], producer: IMatrixProducer<T>): void;
 
   /** Notification that cols have been inserted, removed, and/or replaced in the given matrix. */
-  colsChanged(producer: IMatrixProducer<T>, col: number, numRemoved: number, colsInserted: T[]): void;
+  colsChanged(col: number, numRemoved: number, colsInserted: T[], producer: IMatrixProducer<T>): void;
 
   /** Notification that a range of cells have been replaced in the given matrix. */
-  cellsReplaced(producer: IMatrixProducer<T>, row: number, col: number, numRows: number, numCols: number, values: T[]): void;
+  cellsReplaced(row: number, col: number, numRows: number, numCols: number, values: T[], producer: IMatrixProducer<T>): void;
 }
 
 /** Provides more efficient access to 2D data for vector-aware consumers. */
