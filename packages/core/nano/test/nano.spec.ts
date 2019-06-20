c3c924ad319796410653c36a04724312c10933ef
Jack Williams <jw@jackw.io>
2019-06-20 11:22:43 +0100

    App test

--- packages/core/nano/test/nano.spec.ts
+++ packages/core/nano/test/nano.spec.ts
@@ -73,6 +73,74 @@ describe("nano", () => {
 
     const parseCases = [
         {
+            expression: "+foo.hello(world)",
+            expected: {
+                "start": 0,
+                "end": 17,
+                "op": 9,
+                "expr": {
+                    "start": 1,
+                    "end": 17,
+                    "head": {
+                        "start": 1,
+                        "end": 10,
+                        "left": {
+                            "start": 1,
+                            "end": 4,
+                            "id": "foo"
+                        },
+                        "right": {
+                            "start": 5,
+                            "end": 10,
+                            "label": "hello"
+                        }
+                    },
+                    "args": [
+                        {
+                            "start": 11,
+                            "end": 16,
+                            "id": "world"
+                        }
+                    ]
+                }
+            },
+            errorCount: 0,
+        },
+        {
+            expression: "+foo(hello).world",
+            expected: {
+                "start": 0,
+                "end": 17,
+                "op": 9,
+                "expr": {
+                    "start": 1,
+                    "end": 17,
+                    "left": {
+                        "start": 1,
+                        "end": 11,
+                        "head": {
+                            "start": 1,
+                            "end": 4,
+                            "id": "foo"
+                        },
+                        "args": [
+                            {
+                                "start": 5,
+                                "end": 10,
+                                "id": "hello"
+                            }
+                        ]
+                    },
+                    "right": {
+                        "start": 12,
+                        "end": 17,
+                        "label": "world"
+                    }
+                }
+            },
+            errorCount: 0,
+        },
+        {
             expression: "+foo.hello",
             expected: {
                 "start": 0,
