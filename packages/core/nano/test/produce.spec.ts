71b26dfbfd1d923bef4af29c00a524e62497520e
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-09-19 15:27:47 -0700

    Add tests for cyclic objects

--- packages/core/nano/test/produce.spec.ts
+++ packages/core/nano/test/produce.spec.ts
@@ -1,31 +1,56 @@
 import "mocha";
 import { strict as assert } from "assert";
 import { produce } from "../src/produce";
-import { NullConsumer } from "./util";
+import { nullConsumer } from "./util";
+import { IProducer } from "../src/types";
 
 describe("produce()", () => {
     describe("object", () => {
         it("supports open(), but not openVector()", () => {
             const p = produce({ a: 1 });
             assert.equal("openVector" in p, false);
-            const r = p.open(new NullConsumer());
+            const r = p.open(nullConsumer);
             assert.equal(r.read("a"), 1);
             assert.equal(r.read("b" as any), undefined);
+        });
+
+        it("handles cycles", () => {
+            const root = { child: undefined };
+            const child = { parent: root };
+            root.child = child as any;
+
+            const p = produce(root);
+            const r = p.open(nullConsumer);
+            const childProducer = r.read("child") as unknown as IProducer<any>;
+            const childReader = childProducer.open(nullConsumer);
+            assert.equal(childReader.read("parent"), p);
         });
     })
 
     describe("array", () => {
         it("supports open() and openVector()", () => {
             const p = produce([0]);
-            const r = p.open(new NullConsumer());
+            const r = p.open(nullConsumer);
             assert.equal(r.read("length"), 1);
             assert.equal(r.read("0" as any), 0);
             assert.equal(r.read("1" as any), undefined);
 
-            const v = p.openVector(new NullConsumer());
+            const v = p.openVector(nullConsumer);
             assert.equal(v.length, 1);
             assert.equal(v.read(0), 0);
             assert.equal(v.read(1), undefined);
+        });
+
+        it("handles cycles", () => {
+            const root: any[] = [];
+            const child = [ root ];
+            root.push(child);
+
+            const p = produce(root);
+            const r = p.openVector(nullConsumer);
+            const childProducer = r.read(0);
+            const childReader = childProducer.open(nullConsumer);
+            assert.equal(childReader.read(0), p);
         });
     });
 });
--- packages/core/nano/test/util.ts
+++ packages/core/nano/test/util.ts
@@ -1,7 +1,9 @@
 import { IConsumer, IVectorConsumer, IProducer, IVectorProducer } from "../src/types";
 
-/** A generic test consumer that ignores all change notifications. */
-export class NullConsumer<T> implements IConsumer<T>, IVectorConsumer<T> {
+class NullConsumer<T> implements IConsumer<T>, IVectorConsumer<T> {
     public valueChanged<U extends T, K extends keyof U>(key: K, value: U[K], producer: IProducer<U>): void {}
     public itemsChanged(index: number, numRemoved: number, itemInserted: T[], producer: IVectorProducer<T>): void {}
 }
+
+/** A generic test consumer that ignores all change notifications. */
+export const nullConsumer: IConsumer<unknown> & IVectorConsumer<unknown> = new NullConsumer<unknown>();
