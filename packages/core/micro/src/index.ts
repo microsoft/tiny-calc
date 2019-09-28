497d7f1019890f75e8f4fb6fcaed3c3e238820b2
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-09-28 07:40:09 -0700

    Micro: Export { IMatrix }

--- packages/core/micro/src/index.ts
+++ packages/core/micro/src/index.ts
@@ -1,1 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export { IMatrix } from "./matrix";
 export { ISheetlet, createSheetlet } from "./sheetlet";
